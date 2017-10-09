502aa55441eb29e912240f2c14aedd7e23a7afba
uael <lucas.abel@yandex.com>
2017-10-09 11:39:08 +0200

    ci fixes 9/..

--- test/fs/file.c
+++ test/fs/file.c
@@ -32,11 +32,11 @@ main(void) {
   file = FS_FD_DFT;
   if (fs_file_open(&file, "LICENSE", FS_OPEN_RO) == RET_SUCCESS) {
     char_t buf[256];
-    i64_t r;
+    isize_t r;
 
     while (fs_file_read(&file, buf, 256, &r) == RET_SUCCESS) {
       buf[r] = '\0';
-      printf(buf);
+      puts(buf);
     }
   }
   fs_file_close(&file);
